ade5b80aaaa2e09d1abfadfda8ee872799da3094
Blacktowtw <blackrow.013@gmail.com>
2026-03-22 11:00:15 +0800

    完成多版本malilib模組的依賴設定，並成功建構

---
+++ src/main/java/me/blackrowtw/bk_tools/BKToolsClient.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the BKTools project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2026  BlacKrowtw and contributors
+ *
+ * BKTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BKTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BKTools.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.blackrowtw.bk_tools;
+
+import fi.dy.masa.malilib.event.InitializationHandler;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BKToolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
